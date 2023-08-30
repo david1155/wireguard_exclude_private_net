b0b9ee33dd537572b0fdf1baf48ca703b7470e5e
Sourcery AI <>
2023-08-30 22:30:12 +0000

    'Refactored by Sourcery'

--- generate_subnets.py
+++ generate_subnets.py
@@ -2,9 +2,7 @@ from ipaddress import ip_network
 from config import *
 
 start = '0.0.0.0/0'
-exclude = ['10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16']
-exclude.append(ip_public)
-
+exclude = ['10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16', ip_public]
 result = [ip_network(start)]
 for x in exclude:
     n = ip_network(x)
